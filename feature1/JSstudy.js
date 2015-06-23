005492d648d0c6cdf227f1abc9164f4ac8eb308d
bintianyou <447163450@qq.com>
2015-06-23 19:41:43 +0800

    js基本的互动以及或许对象的方法

---
+++ feature1/JSstudy.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Administrator on 2015/6/23.
+ */
+//常用互动方法
+
+
+/*
+//输出内容
+document.write("nihao");
+//警告消息对话框
+alert("警告");
+//提问消息对话框
+confirm("确定是孙");
+//打开新窗口 window.open(<URL>, <窗口名称>, <参数字符串>)
+//window.open('http://www.imooc.com','_blank','width=300,height=200,menubar=no,toolbar=no, status=no,scrollbars=yes')
+window.open("http://www.apple.com.cn");
+//关闭窗口
+window.close();//关闭本窗口
+mywin=window.open("http://www.baidu.com");
+mywin.close();//关闭指定窗口
+*/
+
+/*
+//DOM基本操作
+//document.getElementsById
+var newh = document.getElementById("title");
+document.write(newh.innerHTML);
+
+//HTML样式 Object.style.property=new style
+var newsty = document.getElementById("con");
+newsty.style.color= "red";
+newsty.style.background="blue";
+
+//显示或者隐藏
+function hide(){
+    document.getElementById("con").style.display="none";
+}
+function show(){
+    document.getElementById("con").style.display="block";
+}
+*/
+
